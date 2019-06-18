f41c33cbd94bf19b73456801ca814010492688c7
Thales Sathler <thales.lima@b2wdigital.com>
2019-06-18 16:38:27 -0300

    rodando o format

--- maas/app.py
+++ maas/app.py
@@ -7,6 +7,6 @@ app = App("", "", "", 1)
 @app.route(["/"], type=RouteTypes.HTTP, methods=["POST"])
 async def operation(request: web.Request):
     data = await request.json()
-    result = data["left"]**data["right"]
+    result = data["left"] ** data["right"]
 
     return web.json_response({"result": result})
